9b68673232f4065b80a1e7ca4c3337783f11842f
Amex-Abiy <amexabiy@gmail.com>
2020-10-24 19:01:25 +0300

    after deploy2

--- server/server.js
+++ server/server.js
@@ -31,19 +31,19 @@ app.use('/api/query', queryRoutes);
 app.use('/api/response', responseRoutes);
 
 // app.use(errorHandler);
-// if (process.env.NODE_ENV === 'production') {
-//     // handle static folder
-//     app.use(express.static(`${__dirname}/public/`))
-//         // handle SPA
-//     app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`))
-// }
+if (process.env.NODE_ENV === 'production') {
+    // handle static folder
+    app.use(express.static(`${__dirname}/public/`))
+        // handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`))
+}
 // handle 404
-// app.use((req, res, next) => {
-//     res.status(404).json({
-//         status: false,
-//         msg: '404! Nothing Found!'
-//     })
-// })
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: false,
+        msg: '404! Nothing Found!'
+    })
+})
 
 
 
