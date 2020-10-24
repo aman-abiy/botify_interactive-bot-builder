03950d44906ff123047e785b0cca300dfa80a804
Amex-Abiy <amexabiy@gmail.com>
2020-10-24 17:02:56 +0300

    ready for heroku

--- server/server.js
+++ server/server.js
@@ -34,4 +34,11 @@ app.use((req, res, next) => {
     })
 })
 
+if (process.env.NODE_ENV === 'production') {
+    // handle static folder
+    app.use(express.static(`${__dirname}/public/`))
+        // handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`))
+}
+
 app.listen(process.env.PORT || 5000);
