a6b27ceb1c6a1a8e8516db3495c84fc43992333b
Amex-Abiy <amexabiy@gmail.com>
2020-10-25 09:39:52 +0300

    5

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -148,20 +148,20 @@ const routes = [{
 
 ]
 
-const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
-})
+// const router = new VueRouter({
+//     mode: 'history',
+//     base: process.env.BASE_URL,
+//     routes
+// })
 
-router.beforeResolve((to, from, next) => {
-    // If this isn't an initial page load.
-    if (to.name) {
-        // Start the route progress bar.
-        NProgress.start()
-    }
-    next()
-})
+// router.beforeResolve((to, from, next) => {
+//     // If this isn't an initial page load.
+//     if (to.name) {
+//         // Start the route progress bar.
+//         NProgress.start()
+//     }
+//     next()
+// })
 
 router.afterEach((to, from) => {
     // Complete the animation of the route progress bar.
